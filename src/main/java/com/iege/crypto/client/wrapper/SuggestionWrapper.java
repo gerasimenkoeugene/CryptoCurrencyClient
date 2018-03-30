6995c51765c03b5bb39fe8c5c18dd5f9668c8041
gerasimenkoeugene <gerasymenkoeugene@gmail.com>
2018-03-30 18:19:27 +0300

    autocomplete

---
+++ src/main/java/com/iege/crypto/client/wrapper/SuggestionWrapper.java
@@ -0,0 +1,20 @@
+package com.iege.crypto.client.wrapper;
+
+
+import java.util.List;
+
+/**
+ * Created by jonaspfeifer on 07.05.17.
+ */
+public class SuggestionWrapper {
+
+  List<Country> suggestions;
+
+  public List<Country> getSuggestions() {
+    return suggestions;
+  }
+
+  public void setSuggestions(List<Country> suggestions) {
+    this.suggestions = suggestions;
+  }
+}
